2e406990a0282e5bb0ca776b3637e51f1d1d3bcd
muskaancodes-hash <muskaan.kumari7908@gmail.com>
2026-08-29 18:09:22 +0530

    Add files via upload

--- todo list python/todo.py
+++ todo list python/todo.py
@@ -31,4 +31,19 @@ elif choice == "3":
     else:
         for i, task in enumerate(tasks, 1):
             print(i, ".", task)   
-    
+elif choice == "2":
+    if len(tasks) == 0:
+        print("No tasks to delete.")
+    else:
+        print("\nYour Tasks:")
+
+        for i in range(len(tasks)):
+            print(i + 1, ".", tasks[i])
+
+        task_number = int(input("Enter task number to delete: "))
+
+        if task_number >= 1 and task_number <= len(tasks):
+            deleted_task = tasks.pop(task_number - 1)
+            print("Task deleted:", deleted_task)
+        else:
+            print("Invalid task number.") 
